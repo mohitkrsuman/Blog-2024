7e01ae22547250f0dfd65ccb16cb952708ee2522
Mohit Kumar Suman <mohitkrsuman@Ritiks-Mac-mini.local>
2024-02-10 16:28:11 +0530

    add controllers and routes

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,3 @@
+export const testApi = (req, res) => {
+    res.json({ message : "Api is working"});
+}
--- api/index.js
+++ api/index.js
@@ -2,16 +2,19 @@ import express from 'express';
 import dotenv from 'dotenv';
 import chalk from 'chalk';
 import dbconnect from './utils/db.js';
+import userRoutes from './routes/user.route.js';
+
+const port = process.env.PORT || 4000;
+const app = express();
 
 dotenv.config();
 dbconnect();
 
-const app = express();
+//The express. json() function is a middleware function used in Express. js applications to parse It is the process of converting a JSON string to a JSON object for data manipulation.
+app.use(express.json());
 
-app.get('/', (req, res) =>{
-   res.send("Hello World");
-})
+app.use('/api', userRoutes);
 
-app.listen(3000, () => {
-   console.log(chalk.blue('Server is running on port 3000'));
+app.listen(port, () => {
+   console.log(chalk.blue(`Server is running on port ${port}`));
 }); 
