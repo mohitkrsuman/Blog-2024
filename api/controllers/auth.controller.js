fc021e1f41b50b55e28848434ac1dbca43ee5051
Mohit Kumar Suman <83830347+mohitkrsuman@users.noreply.github.com>
2024-02-10 22:09:04 +0530

    created signup api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,28 +1,44 @@
-import User from '../models/user.model.js';
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
+  try {
+    const { username, email, password } = req.body;
 
-        if (!username || !email || !password || username === "" || email === "" || password === "") {
-            res.status(400).json({
-                success: false,
-                message: "All fields are required"
-            });
-        }
-        const user = new User({ username, email, password });
-        await user.save();
-
-        res.status(200).json({
-            success: true,
-            message: "User created successfully!"
-        })
-
-    } catch (err) {
-        console.log(err, "Error in signing up");
-        res.status(500).json({
-            success: false,
-            message: err.message
-        });
+    if (
+      !username ||
+      !email ||
+      !password ||
+      username === "" ||
+      email === "" ||
+      password === ""
+    ) {
+      res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
     }
-}
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const user = new User({ 
+        username,
+        email,
+        password : hashedPassword 
+    });
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "User created successfully!",
+      user
+    });
+  } catch (err) {
+    // console.log(err, "Error in signing up");
+    res.status(500).json({
+      success: false,
+      message: "Error in signing up",
+      err
+    });
+  }
+};
